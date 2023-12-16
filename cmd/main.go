dce6bfacff556531f129eceefc5ade9eb926cc6f
Manav-016 <mmanav169@gmail.com>
2023-12-17 02:00:50 +0530

    chore: edit "port" value to be taken from the env vars, for the deployment

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/golang-interview/api"
 )
 
 func main() {
+	// Get the PORT from the environment variable
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Fatal("$PORT must be set")
+	}
+
 	http.HandleFunc("/process-single", api.ProcessSingle)
 	http.HandleFunc("/process-concurrent", api.ProcessConcurrent)
 
 	log.Println("Server is running on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":" + port, nil))
 }
